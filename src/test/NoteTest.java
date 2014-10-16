49e8f27d9e1ebfa10cc27158ee34210c23822038
Geoffrey Liu <liug.2012@gmail.com>
2014-10-16 00:46:02 -0700

    Add Note JUnit Test skeleton

---
+++ src/test/NoteTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Geoffrey
+ *
+ */
+public class NoteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
