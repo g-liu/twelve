46bb1fcce94290b16a2c8d91f0303160a5a63d57
Geoffrey Liu <liug.2012@gmail.com>
2014-12-13 14:59:23 -0800

    application test skeleton

---
+++ src/test/ApplicationTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This unit test class is designed to test the application overall.
+ * @author Geoffrey Liu
+ *
+ */
+public class ApplicationTest {
+
+}
